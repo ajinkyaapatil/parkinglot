94ee68fae75db28dc76143ac22a18405010c1c1d
Ajinkya Arvind Patil <ajinkyaarvindp@Ajinkyas-MacBook-Pro.local>
2024-03-15 22:30:42 +0530

    Slot is initially free to park vehicle

--- src/main/kotlin/Slot.kt
+++ src/main/kotlin/Slot.kt
@@ -1,5 +1,13 @@
 package org.example
 
 class Slot(val id: Int, val vehicleType: VehicleType) {
+    var availability = true
 
+    fun occupy(){
+        availability = false
+    }
+
+    fun free(){
+        availability = true
+    }
 }
--- src/test/kotlin/SlotTest.kt
+++ src/test/kotlin/SlotTest.kt
@@ -5,6 +5,13 @@ import kotlin.test.assertEquals
 
 class SlotTest {
     @Test
+    fun `Slot is initially free to park vehicle`(){
+        val slot = Slot(1, VehicleType.CAR)
+        assertEquals(true, slot.availability)
+    }
+
+
+    @Test
     fun `parking slot created has an ID`() {
         val slot = Slot(1, VehicleType.MOTORCYCLE)
         assertEquals(1, slot.id)
