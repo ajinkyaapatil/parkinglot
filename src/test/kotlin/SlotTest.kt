e89685149d7f98b86df5824db50ed285050da7ee
Ajinkya Arvind Patil <ajinkyaarvindp@Ajinkyas-MacBook-Pro.local>
2024-03-15 22:34:53 +0530

    slot availibilty is tested

--- src/test/kotlin/SlotTest.kt
+++ src/test/kotlin/SlotTest.kt
@@ -9,8 +9,19 @@ class SlotTest {
         val slot = Slot(1, VehicleType.CAR)
         assertEquals(true, slot.availability)
     }
-
-
+    @Test
+    fun `when vehicle is park on a slot its availability is made unavailable`(){
+        val slot = Slot(1, VehicleType.MOTORCYCLE)
+        slot.occupy()
+        assertEquals(false, slot.availability)
+    }
+    @Test
+    fun `when vehicle is unparked on slot it is made available`(){
+        val slot = Slot(1, VehicleType.MOTORCYCLE)
+        slot.occupy()
+        slot.free()
+        assertEquals(true, slot.availability)
+    }
     @Test
     fun `parking slot created has an ID`() {
         val slot = Slot(1, VehicleType.MOTORCYCLE)
