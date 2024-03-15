0f96bd6460ff35a85b72b529cbcf3bb50317fc4b
Ajinkya Arvind Patil <ajinkyaarvindp@Ajinkyas-MacBook-Pro.local>
2024-03-15 22:26:10 +0530

    parking lot creates slots with vehicle type

--- src/main/kotlin/ParkingLot.kt
+++ src/main/kotlin/ParkingLot.kt
@@ -1,6 +1,20 @@
 package org.example
 
-class ParkingLot {
+class ParkingLot(motorCycleCount: Int, carCount: Int, busCount: Int) {
+
+    val slots = mutableMapOf<VehicleType, MutableList<Slot>>(
+        VehicleType.MOTORCYCLE to mutableListOf(),
+        VehicleType.CAR to mutableListOf(),
+        VehicleType.BUS to mutableListOf()
+    )
+
+    init {
+        for(i in 1..motorCycleCount) slots[VehicleType.MOTORCYCLE]?.addLast(Slot(i, VehicleType.MOTORCYCLE))
+        for(i in 1..carCount) slots[VehicleType.CAR]?.addLast(Slot(i, VehicleType.CAR))
+        for(i in 1..busCount) slots[VehicleType.BUS]?.addLast(Slot(i, VehicleType.BUS))
+    }
+
+
     fun findEmptySlotFor(vehicle: Vehicle) : Slot {
         return Slot(1, vehicle.vehicleType)
     }
--- src/test/kotlin/ParkingLotTest.kt
+++ src/test/kotlin/ParkingLotTest.kt
@@ -5,11 +5,22 @@ import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 class ParkingLotTest {
+
     @Test
-    fun `parking lot is able to give a empty slot for a vehicle`(){
+    fun `Parking lot creates 1 Motorcycle, 1 Car and 1 Bus by default`(){
+        val parkingLot = ParkingLot(1, 1, 1)
+
+        assertEquals(1, parkingLot.slots[VehicleType.MOTORCYCLE]?.size)
+        assertEquals(1, parkingLot.slots[VehicleType.CAR]?.size)
+        assertEquals(1, parkingLot.slots[VehicleType.BUS]?.size)
+
+
+    }
+    @Test
+    fun `Parking lot is able to give a empty slot for a vehicle`(){
         val vehicle = Vehicle(VehicleType.MOTORCYCLE)
 
-        val parkingLot = ParkingLot()
+        val parkingLot = ParkingLot(1, 1, 1)
 
         val slot = parkingLot.findEmptySlotFor(vehicle)
 
